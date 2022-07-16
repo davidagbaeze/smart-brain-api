3630a126ad76d6f5192c5c4f9bac11adfe923166
David Agbaeze <you@example.com>
2022-07-16 11:46:05 +0100

    updating database connection

--- server.js
+++ server.js
@@ -12,11 +12,10 @@ const saltRounds = 10;
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    port : 5432,
-    user : 'postgres',
-    password : 'Developer00#',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
